5b138e18792a3f963d086649c6a79e34943ca3c5
moaar <86329664+moaar@users.noreply.github.com>
2021-07-05 10:57:35 -0500

    Update index.js
    test trigger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.f = function(req, res) {
   console.log("Hello to all world, and test trigger 1 ");
+   console.log("Hello to all world, and test trigger 2 ");
   res.send("hello, gcf!");
 };
